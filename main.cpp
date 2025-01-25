e0f8bc43cefd6e86d81d5a2cca3c17570a35c9e3
Master_Procrastinator <69257481+Croionxx@users.noreply.github.com>
2025-01-25 20:49:00 +0530

    Added pos-calc

--- main.cpp
+++ main.cpp
@@ -10,60 +10,33 @@ typedef struct sprite_rectangle{
     char name;
 }sprite_rectangle;
 
-void create_rectangle( sprite_rectangle *rectangle, int row, int col, int name, int pos_x = 0, int pos_y = 0){
-    rectangle->vel_x = row;
-    rectangle->vel_y = col;
+void create_rectangle( sprite_rectangle *rectangle, int pos_x, int pos_y = 0, char name = 'bob', int vel_x = 0, int vel_y = 0){
+    rectangle->vel_x = vel_x;
+    rectangle->vel_y = vel_y;
     rectangle->name = name;
     rectangle->pos_x = pos_x;
     rectangle->pos_y = pos_y;
 }
 
-int print_rectangle(sprite_rectangle *rectangle){
-    for (int i = 0; i < 5; i++){
-        for (int j = 0; j < 5; j++){
-                printf("#");
-            }
+#include <cmath>
 
-        printf("\n");
-    }
-    return 0;   
-}
+struct Vector2D {
+    double x;
+    double y;
+};
 
+Vector2D calculateNewPosition(const Vector2D& initialPosition, 
+                              const Vector2D& initialVelocity, 
+                              const Vector2D& acceleration, 
+                              double time) {
+    Vector2D newPosition;
     
-
-int box(int rows, int cols){
-
-    // Creates a box with the given number of rows and columns
-
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            if (i == 0 || i == rows - 1){
-                if (j == 0 || j == cols - 1){
-                    printf("+");
-                } else {
-                    printf("-");
-                }
-            } else {
-                if (j == 0 || j == cols - 1){
-                    printf("|");
-                } else {
-                    printf(" ");
-                }
-            }
-        }
-        printf("\n");
-    }
-    return 0;
-}
-
-
-int main (int argc, char **argv)
-{
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    box(w.ws_row-5, w.ws_col-5);
+    newPosition.x = initialPosition.x + initialVelocity.x * time + 
+                    0.5 * acceleration.x * time * time;
     
-
-    return 0; 
-
+    newPosition.y = initialPosition.y + initialVelocity.y * time + 
+                    0.5 * acceleration.y * time * time;
+    
+    return newPosition;
 }
+
